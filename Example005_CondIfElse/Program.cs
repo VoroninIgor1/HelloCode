2e8e1222309316ba3bcd618baf9d3e1f5e397908
Igor V <i.v.voronin2000@gmail.com>
2023-01-12 14:28:36 +0300

    Example005_Условие Если

---
+++ Example005_CondIfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+   {Console.WriteLine("Ура! Привет Маша.");}
+else
+   {Console.WriteLine("Привет, " + username + ".");}
