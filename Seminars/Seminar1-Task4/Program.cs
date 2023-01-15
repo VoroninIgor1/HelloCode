9f3f62d6f414d50d44e46828cabe4e9e3985eb88
Igor V <i.v.voronin2000@gmail.com>
2023-01-15 14:16:01 +0300

    ДЗ1. Задачи 2,4,6

--- Seminars/Seminar1-Task4/Program.cs
+++ Seminars/Seminar1-Task4/Program.cs
@@ -1,9 +1,24 @@
 ﻿// принимает на вход три числа и выдаёт максимальное из этих чисел
 Console.WriteLine("Введите 3 числа.");
+Console.Write("a=");
 double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b=");
 double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("c=");
 double c = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine();
-if (a>b&&a>c) Console.WriteLine("max=" + a);
-if (b>a&&b>c) Console.WriteLine("max=" + b);      
-if (c>a&&c>b) Console.WriteLine("max=" + c);
+
+if (a>b) {
+          if (a>c) Console.WriteLine("max=a=" + a);
+          else if (a==c) Console.WriteLine("max=a=c=" + a);
+         }   
+
+if (b>a) {
+          if (b>c) Console.WriteLine("max=b=" + b);
+          else if (b==c) Console.WriteLine("max=b=c=" + b);
+         }   
+
+if (c>b) {if (c>a) Console.WriteLine("max=" + c);}
+    else if (b==a) Console.WriteLine("max=a=b=" + a);
+ 
+if (c==a) {if (c==b) Console.WriteLine("все 3 числа одинаковые, max=a=b=c=" + a);}
