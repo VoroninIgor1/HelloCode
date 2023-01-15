9ac803bb7855807136068e0fb83d6fe343df6dfb
Igor V <i.v.voronin2000@gmail.com>
2023-01-15 13:43:54 +0300

    ДЗ1. Задача4

---
+++ Seminars/Seminar1-Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Введите 3 числа.");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+if (a>b&&a>c) Console.WriteLine("max=" + a);
+if (b>a&&b>c) Console.WriteLine("max=" + b);      
+if (c>a&&c>b) Console.WriteLine("max=" + c);
