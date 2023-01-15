82e9d534a0c0622b2b87774be4b546fb172aeced
Igor V <i.v.voronin2000@gmail.com>
2023-01-15 13:41:48 +0300

    ДЗ1. Задача2

--- Seminars/Seminar1-Task2/Program.cs
+++ Seminars/Seminar1-Task2/Program.cs
@@ -1,10 +1,8 @@
 ﻿// на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
 Console.WriteLine("Введите 2 числа.");
-// int a = int.Parse(Console.ReadLine());
 double a = Convert.ToDouble(Console.ReadLine());
 double b = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine();
 if (a>b) Console.WriteLine("max=" + a + "; min=" + b);
 else if (a<b) Console.WriteLine("max=" + b + "; min=" + a);
 else if (a==b) Console.WriteLine(a + "=" + b);
-
