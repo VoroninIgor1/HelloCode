4fad8965367311ba24d89c2a41052a506fceb9a4
Igor V <i.v.voronin2000@gmail.com>
2023-01-15 14:41:47 +0300

    ДЗ1. Задачи 2,4,6,8

---
+++ Seminars/Seminar1-Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число.");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до указанного числа:");
+int index = 1;
+if (n>0) {while (index<=n) {
+                            if (index %2 == 0) Console.Write(index + "; ");
+                            index++;
+                           }
+         }
+else if (n<0) {while (index>=n) {
+                                if (index %2 == 0) Console.Write(index + "; ");
+                                index--;
+                               }
+              }
+else if (n==0) Console.WriteLine("0");
+
+Console.Write("\b\b.");
