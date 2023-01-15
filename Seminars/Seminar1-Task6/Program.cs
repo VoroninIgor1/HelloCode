ce874e07eed31c8b6450d36b058cf8e3bfb56c36
Igor V <i.v.voronin2000@gmail.com>
2023-01-15 13:51:16 +0300

    ДЗ1. Задачи 2,4,6

---
+++ Seminars/Seminar1-Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿//на вход принимает число и выдаёт, является ли число чётным
+Console.WriteLine("Введите число.");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+if ((a % 2) == 0) Console.WriteLine("число " + a + " - четное");
+else Console.WriteLine("число " + a + " - нечетное");
