d45a7ed01cbb6d6ae4e4e1b031a7378ace84f86d
Igor V <i.v.voronin2000@gmail.com>
2023-01-19 12:42:27 +0300

    ДЗ2. Задача 10,13,15

---
+++ Seminars/Seminar2/Sem2-Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите 3х-значное целое число");
+int a = int.Parse(Console.ReadLine());
+if (a<100 || a>999) Console.WriteLine("Некорректный ввод");
+else {
+    int b = a % 10; 
+    a = (a - b)/10;
+    b = Convert.ToInt32(Math.Sqrt((a % 10)*(a % 10))); 
+    Console.WriteLine(b);}
