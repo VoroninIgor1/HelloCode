ae01793cf811c57240e1282da671bd08016f8549
Igor V <i.v.voronin2000@gmail.com>
2023-01-23 15:11:53 +0300

    ДЗ3. Задача 23

---
+++ Seminars/Seminar3/Sem3-Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Ведите целое число ");
+int num = int.Parse(Console.ReadLine());
+if (num >= 1)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+          Console.Write(Math.Pow(i,3) + ", ");
+        }
+    Console.WriteLine("\b\b ");
+    }
+else { //если введенное число меньше единицы (в т.ч. отрицательное), 
+       //то диапазон от 1 до N включает кубы 1, 0 и далее до N
+        for (int i = 1; i >= num; i--)
+        {
+          Console.Write(Math.Pow(i,3) + ", ");
+        }
+        Console.WriteLine("\b\b ");
+     }
