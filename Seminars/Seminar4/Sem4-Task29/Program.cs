a468a7ad20a95b871e5744daafddbb0bf5176e23
Igor V <i.v.voronin2000@gmail.com>
2023-01-25 12:16:06 +0300

    ДЗ4. Задача 29

---
+++ Seminars/Seminar4/Sem4-Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из заданного числа элементов 
+// принимает значения элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Задайте число элементов массива: ");
+int numarr = int.Parse(Console.ReadLine());
+int[] Arr = new int[numarr];
+
+for(int i=0;i<Arr.Length;i++) //заполнение массива
+    {
+        Console.Write($"Введите {i}й элемент массива: ");
+        Arr[i] = int.Parse(Console.ReadLine());
+    }
+Console.Write("Заданный массив: ");
+for (int i = 0; i < Arr.Length; i++) //вывод массива
+    {
+        Console.Write($"{Arr[i]}; ");
+    }
+Console.WriteLine("\b\b.");
