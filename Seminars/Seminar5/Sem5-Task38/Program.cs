b4743d9b85b75c0a829008dd96927c99fbde5ce8
Igor V <i.v.voronin2000@gmail.com>
2023-01-30 14:44:38 +0300

    ДЗ5. Задача 38

---
+++ Seminars/Seminar5/Sem5-Task38/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 38: Задайте массив целых чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] MakeArr(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0,100); 
+    return array;
+}
+
+int DifMaxMin(int[] array)
+{
+    int dif = 0;
+    int max = array[0];
+    int min = array[0];
+    for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i]<min) min = array[i]; 
+            if (array[i]>max) max = array[i]; 
+        }
+    dif = max - min;    
+    return dif;
+}
+
+Console.WriteLine("введите длину массива");
+int a = int.Parse(Console.ReadLine());
+int[] arr = MakeArr(a);
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+Console.WriteLine("Разница между максимальным и минимальным элементами массива = " + DifMaxMin(arr));
