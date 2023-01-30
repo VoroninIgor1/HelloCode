d97697f440be91ce8270864b06f2c92d96079f6b
Igor V <i.v.voronin2000@gmail.com>
2023-01-30 14:34:30 +0300

    ДЗ5. Задача 36

---
+++ Seminars/Seminar5/Sem5-Task36/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] MakeArr(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-99,100); 
+    return array;
+}
+
+int SumNotEvenInd(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (i%2==1) sum=sum+array[i]; 
+    return sum;
+}
+
+Console.WriteLine("введите длину массива");
+int a = int.Parse(Console.ReadLine());
+int[] arr = MakeArr(a);
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+Console.WriteLine("Сумма эл-ов массива с нечет.индексами = " + SumNotEvenInd(arr));
