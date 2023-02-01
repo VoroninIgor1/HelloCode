aed7046135314d5a62d6283ecae6ad14feb60ef6
Igor V <i.v.voronin2000@gmail.com>
2023-02-01 11:21:10 +0300

    ДЗ6. Задача 41

---
+++ Seminars/Seminar6/Sem6-Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountPosNumArr(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+            if (arr[i] > 0) count++;
+    return count;
+}
+
+Console.WriteLine("Введите число элементов массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите элемент массива с индексом №" + i + ": ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Заданный массив: ");
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("Число элементов в заданном массиве > 0 = " + CountPosNumArr(array));;
