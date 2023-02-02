a55fbebe2f24654740f6c962548447edfe12a45e
VoroninIgor1 <121311258+VoroninIgor1@users.noreply.github.com>
2023-02-02 16:48:08 +0300

    Delete Seminars/Seminar1-Task2 directory

--- Seminars/Seminar1-Task2/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
-Console.WriteLine("Введите 2 числа.");
-double a = Convert.ToDouble(Console.ReadLine());
-double b = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine();
-if (a>b) Console.WriteLine("max=" + a + "; min=" + b);
-else if (a<b) Console.WriteLine("max=" + b + "; min=" + a);
-else if (a==b) Console.WriteLine(a + "=" + b);
