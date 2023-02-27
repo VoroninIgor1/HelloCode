c058ab3762730afcc3c6eaab5708b99f0c9e6486
Igor V <i.v.voronin2000@gmail.com>
2023-02-27 17:47:41 +0300

    Контролоьная работа. Версия 1.Console.WriteLine();

---
+++ Seminars/Test1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит массив из произвольного (заданного пользователем) числа строк произвольной длинны.
+// Написать программу, которая из введенного массива строк выбирает строки, 
+// длинна которых менее или равна длины строки исходного массива с индексом "3"
+// и формирует из них отдельный массив строк
+
+Console.WriteLine("Введите массив строк.");
+Console.Write("Задайте число строк (элементов) массива: n = ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+string[] arr1 = new string[k1];
+Console.WriteLine("Введите " + k1 + " элементов массива через пробел:");
+arr1 = Console.ReadLine().Split(' ');
+
+Console.WriteLine();
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < k1; i++)
+    Console.Write(arr1[i] + "; ");
+    Console.WriteLine("\b\b.");
+
+int k2 = 0; //длинна результирующего массива
+for (int i = 0; i < k1; i++)
+    {
+        if (arr1[i].Length <= arr1[3].Length)
+        k2 = k2 + 1;        
+    }
+
+string[] arr2 = new string[k2];
+int j = 0;
+for (int i = 0; i < k1; i++)
+    {
+        if (arr1[i].Length <= arr1[3].Length)
+        {
+            arr2[j] = arr1[i];
+            j++;
+        }
+    }
+
+Console.WriteLine();
+Console.WriteLine("Полученный массив:");
+for (int i = 0; i < k2; i++)
+    Console.Write(arr2[i] + "; ");
+    Console.WriteLine("\b\b.");
